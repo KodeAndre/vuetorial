d7c1bad3225ad5f3e380bc81ac3bfcd561d3f5dc
kodeandre <andre.kodehode@gmail.com>
2022-05-09 10:29:12 +0200

    Finished and polished

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import { ref, reactive, computed, watch, watchEffect } from 'vue'
-import { default as ChildComp } from './components/ChildComp.vue'
-
-const counter = reactive({
-  count: 0
-})
+import { ref } from 'vue'
+import ChildComp from './components/ChildComp.vue'
+import Todo from './components/Todo.vue'
+import Counter from './components/Counter.vue'
 
 const text = ref('')
 const state = ref(true)
@@ -12,64 +10,20 @@ const message = ref('Hello world!')
 const childTitle = ref('Child title')
 const childMsg = ref('No child message yet')
 
-let id = 0
-
-const newTodo = ref('')
-const hideCompleted = ref(false)
-const todos = ref([
-  { id: id++, text: 'Learn HTML', done: false },
-  { id: id++, text: 'Learn JavaScript', done: false },
-  { id: id++, text: 'Learn Vue', done: false }
-])
-const filteredTodos = computed(() => {
-  return hideCompleted.value ? todos.value.filter(t => !t.done) : todos.value
-})
-
-function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value, done: false })
-  newTodo.value = ''
-}
-function removeTodo(todo: any) {
-  todos.value = todos.value.filter((t) => t !== todo)
-}
 function toggle() {
   state.value = !state.value
 }
-function increment() {
-  counter.count++
-}
-function decrement() {
-  counter.count--
-}
-watch(counter, (value) => {
-  console.log("counterValue" ,value.count)
-})
 </script>
 
 <template>
   <h1>{{ message }}</h1>
-  <p>Count is: {{ counter.count }}</p>
-  <button @click="increment">Increment</button>
-  <button @click="decrement">Decrement</button>
+  <Counter />
   <input placeholder="Write your own text" v-model="text" />
   <p>{{ text }}</p>
   <button @click="toggle">toggle state</button>
   <p v-if="state">Default</p>
   <p v-else>Toggled</p>
-  <form @submit.prevent="addTodo">
-    <input v-model="newTodo" />
-    <button>Add Todo</button>
-  </form>
-  <ul>
-    <li v-for="todo in filteredTodos" :key="todo.id">
-      <input type="checkbox" v-model="todo.done">
-      <span :class="{ done: todo.done }">{{ todo.text }}</span>
-      <button @click="removeTodo(todo)">X</button>
-    </li>
-  </ul>
-  <button @click="hideCompleted = !hideCompleted">
-    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
-  </button>
+  <Todo />
   <ChildComp @response="(msg) => childMsg = msg" :msg="childTitle">Slotted text</ChildComp>
   <p>{{ childMsg }}</p>
 </template>
---
+++ src/components/Counter.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { reactive, watch } from 'vue'
+const counter = reactive({
+  count: 0
+})
+function increment() {
+  counter.count++
+}
+function decrement() {
+  counter.count--
+}
+watch(counter, (value) => {
+  console.log("counterValue" ,value.count)
+})
+</script>
+
+<template>
+  <p>Count is: {{ counter.count }}</p>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+</template>
